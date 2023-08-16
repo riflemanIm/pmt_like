057d87fc546527009d59a3d99fb5ab138913d22d
Oleg La <oleglambin@gmail.com>
2023-08-16 17:03:21 +0300

    feat tarifs

--- src/components/dashboard/ProductTariffs.js
+++ src/components/dashboard/ProductTariffs.js
@@ -1,16 +1,18 @@
 import React from "react";
 import {
-  Typography,
   Box,
   Table,
   TableBody,
   TableCell,
   TableHead,
   TableRow,
-  Chip,
+  Typography,
 } from "@mui/material";
-import BaseCard from "../baseCard/BaseCard";
-
+import ComputerIcon from "@mui/icons-material/Computer";
+import DnsIcon from "@mui/icons-material/Dns";
+import DeveloperBoardIcon from "@mui/icons-material/DeveloperBoard";
+import LensIcon from "@mui/icons-material/Lens";
+import RadioButtonUncheckedIcon from "@mui/icons-material/RadioButtonUnchecked";
 const products = [
   {
     name: "Sunil Joshi",
@@ -48,91 +50,453 @@ const products = [
 
 const ProductTariffs = () => {
   return (
-    <Table
-      aria-label="simple table"
-      sx={{
-        mt: 3,
-        whiteSpace: "nowrap",
-      }}
-    >
-      <TableHead>
-        <TableRow>
-          <TableCell>
-            <Typography color="textSecondary" variant="h6">
-              Модуль / опция
-            </Typography>
-          </TableCell>
-          <TableCell>
-            <Typography color="textSecondary" variant="h6">
-              Name
-            </Typography>
-          </TableCell>
-          <TableCell>
-            <Typography color="textSecondary" variant="h6">
-              Priority
-            </Typography>
-          </TableCell>
-          <TableCell align="right">
-            <Typography color="textSecondary" variant="h6">
-              Budget
-            </Typography>
-          </TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {products.map((product) => (
-          <TableRow key={product.name}>
+    <>
+      {" "}
+      <Table
+        aria-label="simple table"
+        sx={{
+          mt: 3,
+          whiteSpace: "nowrap",
+        }}
+      >
+        <TableHead>
+          <TableRow>
             <TableCell>
-              <Box
-                sx={{
-                  display: "flex",
-                  alignItems: "center",
-                }}
+              Модуль /{" "}
+              <span
+                sx={(theme) => ({
+                  color: theme.palette.primary.dark,
+                })}
               >
-                <Box>
-                  <Typography
-                    variant="h6"
-                    sx={{
-                      fontWeight: "600",
-                    }}
-                  >
-                    {product.name}
-                  </Typography>
-                  <Typography
-                    color="textSecondary"
-                    sx={{
-                      fontSize: "13px",
-                    }}
-                  >
-                    {product.post}
-                  </Typography>
-                </Box>
-              </Box>
+                опция
+              </span>
             </TableCell>
-            <TableCell>
-              <Typography color="textSecondary" variant="h6">
-                {product.pname}
-              </Typography>
-            </TableCell>
-            <TableCell>
-              <Chip
-                sx={{
-                  pl: "4px",
-                  pr: "4px",
-                  backgroundColor: product.pbg,
-                  color: "#fff",
-                }}
-                size="small"
-                label={product.priority}
-              ></Chip>
-            </TableCell>
-            <TableCell align="right">
-              <Typography variant="h6">${product.budget}k</Typography>
-            </TableCell>
+            <TableCell>Тип лицензии</TableCell>
+            <TableCell>Compact</TableCell>
+            <TableCell>Standart</TableCell>
+            <TableCell>Enterprise</TableCell>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHead>
+        <TableBody>
+          <TableRow>
+            <TableCell>Системное ядро</TableCell>
+            <TableCell>
+              <ComputerIcon />
+            </TableCell>
+            <TableCell>
+              <LensIcon />
+            </TableCell>
+            <TableCell>
+              <LensIcon />
+            </TableCell>
+            <TableCell>
+              <LensIcon />
+            </TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Статистика</TableCell>
+            <TableCell>
+              <ComputerIcon />
+            </TableCell>
+            <TableCell>
+              <LensIcon />
+            </TableCell>
+            <TableCell>
+              <LensIcon />
+            </TableCell>
+            <TableCell>
+              <LensIcon />
+            </TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Расписание</TableCell>
+            <TableCell>
+              <ComputerIcon />
+            </TableCell>
+            <TableCell>
+              <LensIcon />
+            </TableCell>
+            <TableCell>
+              <LensIcon />
+            </TableCell>
+            <TableCell>
+              <LensIcon />
+            </TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Удаленный доступ</TableCell>
+            <TableCell>
+              <ComputerIcon />
+            </TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Учет звонков</TableCell>
+            <TableCell>
+              <ComputerIcon />
+            </TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Интеграция с call-центром</TableCell>
+            <TableCell>
+              <DnsIcon />
+            </TableCell>
+            <TableCell></TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>ЭМК</TableCell>
+            <TableCell>
+              <ComputerIcon />
+            </TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Интеграция с проф.диктофонами</TableCell>
+            <TableCell>
+              <ComputerIcon />
+            </TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell> Экспорт ЭМК в HTML формате</TableCell>
+            <TableCell>
+              <ComputerIcon />
+            </TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Лаборатория</TableCell>
+            <TableCell>
+              <ComputerIcon />
+            </TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell>
+              <LensIcon />
+            </TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Учет услуг</TableCell>
+            <TableCell>
+              <ComputerIcon />
+            </TableCell>
+            <TableCell>
+              <LensIcon />
+            </TableCell>
+            <TableCell>
+              <LensIcon />
+            </TableCell>
+            <TableCell>
+              <LensIcon />
+            </TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Касса</TableCell>
+            <TableCell>
+              <ComputerIcon />
+            </TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Интеграция с бух.системой</TableCell>
+            <TableCell>
+              <DnsIcon />
+            </TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell>
+              <LensIcon />
+            </TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Аптека</TableCell>
+            <TableCell>
+              <ComputerIcon />
+            </TableCell>
+            <TableCell></TableCell>
+            <TableCell>
+              <LensIcon />
+            </TableCell>
+            <TableCell>
+              <LensIcon />
+            </TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Розничная аптека</TableCell>
+            <TableCell>
+              <ComputerIcon />
+            </TableCell>
+            <TableCell></TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Коечный фонд</TableCell>
+            <TableCell>
+              <ComputerIcon />
+            </TableCell>
+            <TableCell></TableCell>
+            <TableCell></TableCell>
+            <TableCell>
+              <LensIcon />
+            </TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Репликация</TableCell>
+            <TableCell>
+              <DnsIcon />
+            </TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Обработка изображений</TableCell>
+            <TableCell>
+              <ComputerIcon />
+            </TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell>
+              <LensIcon />
+            </TableCell>
+            <TableCell>
+              <LensIcon />
+            </TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Поддержка карт видеозахвата</TableCell>
+            <TableCell>
+              <ComputerIcon />
+            </TableCell>
+            <TableCell></TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Протокол DICOM</TableCell>
+            <TableCell>
+              <DnsIcon />
+            </TableCell>
+            <TableCell></TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell>
+              <LensIcon />
+            </TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Интеграция с приборами по DICOM</TableCell>
+            <TableCell>
+              <DeveloperBoardIcon />
+            </TableCell>
+            <TableCell></TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>METASCAN</TableCell>
+            <TableCell>
+              <DnsIcon />
+            </TableCell>
+            <TableCell></TableCell>
+            <TableCell>
+              <LensIcon />
+            </TableCell>
+            <TableCell>
+              <LensIcon />
+            </TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Однонаправленный драйвер</TableCell>
+            <TableCell>
+              <DeveloperBoardIcon />
+            </TableCell>
+            <TableCell></TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Двунаправленный драйвер</TableCell>
+            <TableCell>
+              <DeveloperBoardIcon />
+            </TableCell>
+            <TableCell></TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Драйвер на основе ASTM</TableCell>
+            <TableCell>
+              <DeveloperBoardIcon />
+            </TableCell>
+            <TableCell></TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Протокол HL7</TableCell>
+            <TableCell>
+              <DeveloperBoardIcon />
+            </TableCell>
+            <TableCell></TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Интеграция с PACS</TableCell>
+            <TableCell>
+              <DeveloperBoardIcon />
+            </TableCell>
+            <TableCell></TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell>
+              <RadioButtonUncheckedIcon />
+            </TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+      <Box my={2} sx={{ display: "flex" }}>
+        <LensIcon />
+        <Typography variant="body2" ml={1}>
+          - Включено в стандартный набор тарифного плана
+        </Typography>
+      </Box>
+      <Box my={2} sx={{ display: "flex" }}>
+        <RadioButtonUncheckedIcon />
+        <Typography variant="body2" ml={1}>
+          - Можно приобрести дополнительно к набору
+        </Typography>
+      </Box>
+    </>
   );
 };
 
