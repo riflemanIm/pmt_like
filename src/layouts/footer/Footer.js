a51120fd0bf74e08ef4f1b2405d2e7b4195c41b6
Oleg La <oleglambin@gmail.com>
2023-08-18 01:51:50 +0300

    fix: bottom color

--- src/layouts/footer/Footer.js
+++ src/layouts/footer/Footer.js
@@ -3,7 +3,7 @@ import { Box, Typography } from "@mui/material";
 
 const Footer = () => {
   return (
-    <Box sx={{ p: 3, textAlign: "center" }}>
+    <Box sx={{ p: 3, textAlign: "center", color: "#fff" }}>
       <Typography>© 2023 All rights reserved by PMT</Typography>
     </Box>
   );
