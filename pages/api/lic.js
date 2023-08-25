02c7a3d49ddcfa53de899cdf78081a1d1931d4c2
Oleg La <oleglambin@gmail.com>
2023-08-25 13:09:45 +0300

    dev

--- pages/api/lic.js
+++ pages/api/lic.js
@@ -18,20 +18,25 @@ export default async function handler(req, res) {
   try {
     let pool = await sql.connect(config);
 
-    let result1 = await pool.request().query("select * from temp ");
+    // let result1 = await pool.request().query("select * from temp ");
 
-    console.dir(result1);
+    // console.dir(result1);
 
     // // Stored procedure
 
-    // let result2 = await pool
-    //   .request()
-    //   .input("input_parameter", sql.Int, value)
-    //   .output("output_parameter", sql.VarChar(50))
-    //   .execute("procedure_name");
+    let result2 = await pool
+      .request()
+      .input("@uLogin", sql.VarChar(30), "support")
+      .input("@uPassword", sql.VarChar(30), "pmtsupport")
+      .input("@uIP", sql.VarChar(30), "46.72.64.63")
+      .input("@uVersion", sql.VarChar(30), "8.105")
+      .input("@uDBCode", sql.VarChar(30), "ABCDEF09")
+
+      //.output("output_parameter", sql.VarChar(250))
+      .execute("GenerateRescueLicenseWeb");
 
     //console.dir(result2);
-    res.status(200).json({ pool });
+    res.status(200).json({ result2 });
   } catch (err) {
     res.status(500).json({ message: err.message });
 
