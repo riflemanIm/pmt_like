80461f047a5e13bf29bf3d86cab6272809a77cd8
Oleg La <oleglambin@gmail.com>
2023-09-21 19:37:10 +0300

    fix: text serverResponse

--- src/actions/user.js
+++ src/actions/user.js
@@ -21,7 +21,7 @@ export async function loginUser(dispatch, login, password) {
         else
           dispatch({
             type: "SET_SERVER_RESPONSE",
-            payload: { serverResponse: "Пользователь не найден" },
+            payload: { serverResponse: "Комбинация логин или пароль не верна" },
           });
       })
       .catch((err) => {
