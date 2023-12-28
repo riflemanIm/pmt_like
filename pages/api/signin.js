e6ad931b00d16159e994758bca271f47e1f7cb16
Oleg La <oleglambin@gmail.com>
2023-12-28 20:14:41 +0300

    fix: gen jwt

--- pages/api/signin.js
+++ pages/api/signin.js
@@ -1,8 +1,8 @@
 import { q } from "../../src/lib/db";
 import isEmpty, { password } from "../../src/helpers";
 import { sign } from "jsonwebtoken";
-import { setCookie } from "cookies-next";
-import md5 from "md5";
+// import { setCookie } from "cookies-next";
+// import md5 from "md5";
 
 export default async function handler(req, res) {
   //const route = req.body.route;
@@ -72,16 +72,18 @@ export default async function handler(req, res) {
       //   maxAge: cookieExpiresIn,
       // });
       // // -------------- END add cookies like in last project in PHP  --------------
+
       const payload = {
         sub: user.login,
-        nonce: `${user.login}${user.login}`,
+        nonce: `${user.id}${user.login}`,
         given_name: user.name.split(" ")[0],
         family_name: user.name.split(" ")[1],
         email: user.email,
       };
+
       const privateKey =
         "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2afgN52RvKYLyL+MF5FLBF09ql0wZW/qZ1Hc3IajHzY0dp9U4eN34G3KY1xIPZTE55ml9UkhkkoL+U1kEBkHTC0hgwi9Z9Tn6f+VPyHodR/BuslzUq2D1QGtEsJN4TNBEhgqNhvdByYUT5XCDr+g3Z6DPg63TrvUKI90yUO4MZVEiBjxXuzfRtpjfLqox3W4/TI5x9LG8gGduN4AE3rRRQRobwtaQ8I6qx/gb5CncPUf7OVli8BTYVT3g7twgzUg12+3P3EoBcbskKg7KnES8QJMwq5NULGSxMFV7u8oaipP6EjCcaBLRtieYxXOvHq9H7xgsyTnd/pBEDUyGmMscQIDAQAB";
-      const tokenFD = sign(payload, privateKey, { algorithm: "RS256" });
+      const tokenFD = sign(payload, privateKey);
 
       console.log({ ...user, tokenFD });
       res.status(200).json({ ...user, tokenFD });
