1274654cf9528d98152af4bf686ba8bb9a76b05a
Oleg La <oleglambin@gmail.com>
2024-01-12 13:46:33 +0300

    getServerSideProps

--- pages/api/signin.js
+++ pages/api/signin.js
@@ -86,6 +86,7 @@ export default async function handler(req, res) {
         if (!resRedir.request.res.responseUrl) {
           throw new Error("resurlt Redirect responseUrl in empty");
         }
+        res.status(200).json({ ...user, redirectUrl: resRedir });
 
         const queryString = resRedir.request.res.responseUrl.split("?")[1];
         const nonce = getParam(queryString, "nonce");
