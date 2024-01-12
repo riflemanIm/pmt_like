45e2c4ad3bb202a2b0703f9e6582fb90f8dbad55
Oleg La <oleglambin@gmail.com>
2024-01-12 13:42:53 +0300

    getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -643,62 +643,39 @@ export default function Solution({ menu }) {
     </FullLayout>
   );
 }
-// export async function getServerSideProps(context) {
-//   const { query } = context;
-//   const authClientUrl =
-//     "https://medialog.myfreshworks.com/login/auth/1703779775100?client_id=451979510707337272&redirect_uri=https%3A%2F%2Fmedialog.freshdesk.com%2Ffreshid%2Fcustomer_authorize_callback%3Fhd%3Dsupport.medialog.ru";
-//   try {
-//     const qqq = await axios.get(authClientUrl);
+export async function getServerSideProps(context) {
+  const { query } = context;
+  if (query.nonce) {
+    const toDate = new Date().getTime();
+    const payload = {
+      sub: "4799",
+      iat: toDate,
+      nonce: query.nonce,
+      email: "osipchuk@postmodern.ru",
+      name: "Илья Осипчук",
+    };
 
-//     if (qqq.request.res) {
-//       const resRedir = await axios.get(qqq.request.res.responseUrl);
-//       console.log("resRedir", resRedir.request.res.responseUrl);
-//       const queryString = resRedir.request.res.responseUrl.split("?")[1];
-//       const nonce = getParam(queryString, "nonce");
-//       const state = getParam(queryString, "state");
-//       const client_id = getParam(queryString, "client_id");
-//       const redirect_uri = getParam(queryString, "redirect_uri");
+    try {
+      const privateKey = fs.readFileSync("./data/jwtRS256.key");
+      const id_token = sign(payload, privateKey, {
+        expiresIn: "6h",
+        algorithm: "RS256",
+        allowInsecureKeySizes: true,
+      });
 
-//       if (nonce && state) {
-//         const toDate = new Date().getTime();
-//         const payload = {
-//           sub: "4799",
-//           iat: toDate,
-//           nonce: nonce,
-//           email: "osipchuk@postmodern.ru",
-//           name: "Илья Осипчук",
-//         };
+      const redirectUrl = `${query.redirect_uri}?state=${query.state}&nonce=${query.nonce}&id_token=${id_token}&client_id=${query.client_id}`;
 
-//         try {
-//           const privateKey = fs.readFileSync("./data/jwtRS256.key");
-//           const id_token = sign(payload, privateKey, {
-//             expiresIn: "6h",
-//             algorithm: "RS256",
-//             allowInsecureKeySizes: true,
-//           });
+      console.log("redirectUrl", redirectUrl);
+      return {
+        redirect: {
+          permanent: false,
+          destination: redirectUrl,
+        },
+      };
+    } catch (error) {
+      console.log("error", error);
+    }
+  }
 
-//           const redirectUrl = `${redirect_uri}?state=${state}&nonce=${nonce}&id_token=${id_token}&client_id=${client_id}`;
-
-//           console.log("redirectUrl", redirectUrl);
-//           return {
-//             redirect: {
-//               permanent: false,
-//               destination: redirectUrl,
-//             },
-//           };
-//         } catch (error) {
-//           console.log("error", error);
-//         }
-//       } else {
-//         throw new Error("nonce && state in empty");
-//       }
-//     }
-//     {
-//       throw new Error("responseUrl in empty");
-//     }
-//   } catch (error) {
-//     console.log("getServerSideProps error", error);
-//   }
-
-//   return { props: { ok: "ok" } };
-// }
+  return { props: { ok: "ok" } };
+}
