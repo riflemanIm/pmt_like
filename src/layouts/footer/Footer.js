d36e0b00f7c11f9ab8df45a3b37c00ef6bff89bd
Oleg La <oleglambin@gmail.com>
2024-05-23 16:30:19 +0300

    fix: add link in footer

--- src/layouts/footer/Footer.js
+++ src/layouts/footer/Footer.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Link, Typography } from "@mui/material";
 import { getYearNow } from "../../helpers";
 import { makeStyles } from "@mui/styles";
 
 const useStyles = makeStyles((theme) => ({
   link: {
     color: theme.palette.primary.dark,
+    textDecoration: "none",
   },
 }));
 
@@ -14,13 +15,13 @@ const Footer = () => {
   return (
     <Box sx={{ p: 3, textAlign: "center" }}>
       <Typography my={3}>
-        <a
+        <Link
           href="https://reestr.digital.gov.ru/reestr/303659/"
           target="_blank"
           className={classes.link}
         >
           Реестровая запись №2309 от 15.12.2016 в РПО
-        </a>
+        </Link>
       </Typography>
       <Typography color="#fff">
         © {getYearNow()} All rights reserved by PMT
