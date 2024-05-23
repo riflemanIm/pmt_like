469154729721c8e888b52efb199a87580c6b2f06
Oleg La <oleglambin@gmail.com>
2024-05-23 16:24:55 +0300

    fix: add link in footer

--- src/layouts/footer/Footer.js
+++ src/layouts/footer/Footer.js
@@ -3,10 +3,9 @@ import { Box, Typography } from "@mui/material";
 import { getYearNow } from "../../helpers";
 import { makeStyles } from "@mui/styles";
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   link: {
-    textShadow:
-      "0 0 5px #FFF, 0 0 10px #FFF, 0 0 15px #FFF, 0 0 20px #F6F7F9, 0 0 30px #F6F7F9, 0 0 40px #F6F7F9, 0 0 55px #F6F7F9, 0 0 75px #F6F7F9",
+    color: theme.palette.primary.dark,
   },
 }));
 
@@ -15,7 +14,11 @@ const Footer = () => {
   return (
     <Box sx={{ p: 3, textAlign: "center" }}>
       <Typography my={3}>
-        <a href="https://reestr.digital.gov.ru/reestr/303659/" target="_blank">
+        <a
+          href="https://reestr.digital.gov.ru/reestr/303659/"
+          target="_blank"
+          className={classes.link}
+        >
           Реестровая запись №2309 от 15.12.2016 в РПО
         </a>
       </Typography>
