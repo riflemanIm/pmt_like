b04774222c29c1980272a195c32c2a24ae50a32d
Oleg La <oleglambin@gmail.com>
2025-01-31 21:12:30 +0300

    fix:lic (back)

--- pages/api/lic.js
+++ pages/api/lic.js
@@ -1,5 +1,5 @@
 import { q } from "../../src/lib/db";
-import isEmpty, { clientIp } from "../../src/helpers";
+import isEmpty from "../../src/helpers";
 import SENDMAIL from "../../src/helpers/mail";
 const sql = require("mssql");
 import { verify } from "jsonwebtoken";
@@ -21,11 +21,11 @@ export default async function handler(req, res) {
       query: querySql,
       values: [decoded.id, decoded.login, decoded.name],
     });
-
+    //console.log("req.body", req.body);
     if (isEmpty(result)) {
       res.status(401).json({ message: "The user is not logged in" });
     }
-    /** --------- gettingn Rescue License fron mssql -------------- */
+    /** --------- gettingn Rescue or Demo License fron mssql -------------- */
     const config = {
       user: "pmtsite",
       password: "licretreiver",
@@ -37,19 +37,22 @@ export default async function handler(req, res) {
     };
 
     let pool = await sql.connect(config);
-    const ip = clientIp(req);
-    console.log("==ip==", ip);
-
+    const procedureName =
+      req.body.lic === "rescue"
+        ? "GenerateRescueLicenseWeb"
+        : "GenerateDemoLicenseWeb";
+    const uDBCodeLength = req.body.lic === "rescue" ? 30 : 45;
     result = await pool
       .request()
       .input("uLogin", sql.VarChar(30), req.body.login)
       .input("uPassword", sql.VarChar(30), req.body.password)
-      .input("uIP", sql.VarChar(30), ip)
+      .input("uIP", sql.VarChar(30), req.body.ip)
       .input("uVersion", sql.VarChar(30), req.body.version)
-      .input("uDBCode", sql.VarChar(30), req.body.code)
-      .execute("GenerateRescueLicenseWeb");
+      .input("uDBCode", sql.VarChar(uDBCodeLength), req.body.code)
+      .execute(procedureName);
 
     if (result.recordset[0].ExitCode) {
+      //console.log("result", result.recordset[0]);
       throw new Error(
         "В настоящее время получить лицензию невозможно. Повторите попытку позже.\n"
       );
@@ -59,7 +62,7 @@ export default async function handler(req, res) {
 
     const options = {
       from: `${req.body.user.login}<${req.body.user.email}>`, // sender address
-      to: "oleglambin@gmail.com", // receiver email
+      to: req.body.user.email, // receiver email
       subject: "Site send GenerateRescueLicenseWeb", // Subject line
       text: result.recordset[0].DemoLicense,
       //html: HTML_TEMPLATE(req.body.message),
